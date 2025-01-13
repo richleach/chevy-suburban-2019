4d057e2d396dd31868ec3aa19ddf20dffcc9879e
Rich Leach <rich@cfsnap.com>
2025-01-13 15:14:04 -0700

    initial commit to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,151 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <div className="flex flex-col min-h-screen items-center justify-start">
+            {/* Navbar */}
+            <nav className="flex justify-evenly items-center w-full bg-blue-500 text-white p-4 text-center shadow-md">
+                <div>
+                    roxparkdavid@gmail.com
+                </div>
+                <div>
+                    <h1 className="text-xl font-bold">Chevy Suburban 2019</h1>
+                </div>
+                <div>
+                    720-425-2327
+                </div>
+            </nav>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {/* Main Content */}
+            <div className="flex-grow flex flex-col items-center  w-full p-6">
+                <div className="bg-gray-100 w-full max-w-4xl p-4 rounded-md shadow-md">
+                    <h2 className="text-2xl font-semibold mb-4 text-center">
+                        2019 Chevy Suburban ½ Ton Z71 Midnight Edition Luxury Package
+
+                    </h2>
+                    <div className="p-3">
+                        <ul>
+                            <li>32,300 MILES</li>
+                            <li> ORIGINAL OWNER</li>
+                            <li>New KO2s</li>
+                            <li>New WeatherTech mats</li>
+                            <li>2 TV screens, sunroof</li>
+                            <li>Leather throughout</li>
+                            <li>Heated, adjustable mid-row captain chairs</li>
+                            <li> Too much to list, see window sticker!</li>
+                        </ul>
+                        <br/><br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/frontLeft.png" width={500} height={500} alt="Front Left"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/frontRight.png" width={500} height={500} alt="Front Right"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/interior1.png" width={500} height={500} alt="Interior"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/interior2.png" width={500} height={500} alt="Interior, side"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/interior3.png" width={500} height={500} alt="Interior, side2"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/rearLeft.png" width={500} height={500} alt="Rear Left"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/rearRight.png" width={500} height={500} alt="Rear Right"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/rearTailgateInterior.png" width={500} height={500}
+                                   alt="Rear Tailgate Interior"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/tire1.png" width={500} height={500} alt="Tires"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/mileage.png" width={500} height={500} alt="Mileage"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+                        <div className="flex items-center justify-center">
+                            <Image src="/images/sticker01.png" width={600} height={600} alt="Sticker"
+                                   style={{
+                                       borderRadius: "20px",
+                                       border: "none",
+                                       boxShadow: "0 0 0 2px rgba(0,0,0,1)"
+                                   }}/>
+                        </div>
+                        <br/>
+
+                    </div>
+
+
+                </div>
+            </div>
+
+            {/* Bottom Pane */}
+            <footer className="w-full bg-gray-200 py-8 flex justify-center items-center">
+                Last Updated 01-14-2025
+            </footer>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
